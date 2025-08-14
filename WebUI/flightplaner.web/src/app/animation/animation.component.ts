d654897f9bd33bb45ae9d1eff6ab107ae75bc992
hau-bold <michael-krasser@t-online.de>
2025-08-14 19:04:06 +0200

    [FE] started to move an airplane

--- WebUI/flightplaner.web/src/app/animation/animation.component.ts
+++ WebUI/flightplaner.web/src/app/animation/animation.component.ts
@@ -21,11 +21,14 @@ export class AnimationComponent implements AfterViewInit {
   private mercatorCoordinates: Point[] = [];
   private backgroundImageUrl = '../../assets/M1.jpg';
   private backgroundImageUrl2 = '../../assets/M2.jpg';
+  private planeImagePath='../../assets/final-Photoroom.png';
+  private plane: HTMLImageElement;
   private toggleBackground: boolean = true;
   public showRoute: boolean = false;
   public routeClickLabel: string = 'Route';
-
-  http = inject(HttpClient);
+  public flightLabel:string ='Start';
+  private currentPlaneLocation: Point = { xPx: 0, yPx: 0, city: '' ,isStart:false};
+  private targetsIndex: number=1;
 
   radius = 10;
   offset = 2;
@@ -33,6 +36,7 @@ export class AnimationComponent implements AfterViewInit {
   private offscreenCanvas: HTMLCanvasElement;
   private offscreenContext: CanvasRenderingContext2D | null;
   public isAnimating: boolean = false;
+  public isFlying:boolean = false;
   public imgWidth: number = 0;
   public imgHeight: number = 0;
 
@@ -42,10 +46,16 @@ export class AnimationComponent implements AfterViewInit {
   constructor( @Inject(CoordinatesService) private readonly coordinatesService: CoordinatesService) {
     this.offscreenCanvas = document.createElement('canvas');
     this.offscreenContext = this.offscreenCanvas.getContext('2d');
+    this.plane = new Image();
+    this.plane .src = this.planeImagePath;
+    this.plane.onload = () => {
+    console.log('Plane image loaded');
+  };
   }
 
   ngAfterViewInit(): void {
     this.processImage();
+   
   }
 
   public storeGuess(event: MouseEvent) {
@@ -81,7 +91,10 @@ this.coordinatesService.GetMercatorCoordinates(canvas.width, canvas.height, this
             .subscribe(points => {
               this.mercatorCoordinates = points;
               this.drawNeedles(this.mercatorCoordinates);
+               this.currentPlaneLocation = this.mercatorCoordinates[0];
+              this.targetsIndex=1;
             });
+             
         }
       }
     };
@@ -121,7 +134,7 @@ this.coordinatesService.GetMercatorCoordinates(canvas.width, canvas.height, this
             context.drawImage(this.offscreenCanvas, 0, 0);
           }
 
-          this.animateAllTargets();
+          this.startAnimationLoop();
         });
 
 
@@ -131,7 +144,14 @@ this.coordinatesService.GetMercatorCoordinates(canvas.width, canvas.height, this
     }
   }
 
-private async animateAllTargets(): Promise<void> {
+private async startAnimationLoop(): Promise<void> {
+  if (this.animationId) 
+  {
+    return;
+  }
+
+
+
   const canvas = this.myCanvas.nativeElement;
   const context = canvas.getContext('2d');
   if (!context) return;
@@ -152,7 +172,10 @@ private async animateAllTargets(): Promise<void> {
   const frameInterval = 150;
 
   const animateFrame = (timestamp: number) => {
-    if (!this.isAnimating) return;
+    if (!this.isAnimating && !this.isFlying) 
+      {
+        return;
+      }
 
     const elapsed = timestamp - lastRenderTime;
     if (elapsed >= frameInterval) {
@@ -166,6 +189,13 @@ private async animateAllTargets(): Promise<void> {
         this.drawRoute(context, routePoints);
       }
 
+     // move plane
+      if (this.isFlying && this.targetsIndex < this.mercatorCoordinates.length) {
+        const isMoving = this.movePlaneto(this.mercatorCoordinates[this.targetsIndex], context);
+        if (!isMoving) this.targetsIndex++;
+      }
+
+      // Animate targets
       this.targetStates.forEach(({ point, state }) => {
         context.beginPath();
         context.arc(point.xPx, point.yPx, state.radius, 0, 2 * Math.PI);
@@ -193,6 +223,40 @@ private async animateAllTargets(): Promise<void> {
   this.animationId = requestAnimationFrame(animateFrame);
 }
 
+private movePlaneto(target: Point,context: CanvasRenderingContext2D): boolean
+{
+console.log('move plae to called:');
+
+const dx = target.xPx - this.currentPlaneLocation.xPx;
+const dy = target.yPx - this.currentPlaneLocation.yPx;
+ 
+const distance = Math.sqrt(dx * dx + dy * dy);
+
+if(distance < 1) {
+  return false; 
+}
+
+// calc new position:
+
+if (dx > 0) {
+  this.currentPlaneLocation.xPx += 1;
+} else if (dx < 0) {
+  this.currentPlaneLocation.xPx -= 1;
+}
+
+if (dy > 0) {
+  this.currentPlaneLocation.yPx += 1;
+} else if (dy < 0) {
+  this.currentPlaneLocation.yPx -= 1;
+}
+
+const angle =this.getAngleBetweenPoints(this.currentPlaneLocation,target);
+
+// draw at (new) current posdition
+this.drawRotatedPlane(context,this.plane,this.currentPlaneLocation.xPx,this.currentPlaneLocation.yPx, angle);
+
+return true;
+}
 
   public stopAnimation(): void {
     this.isAnimating = false;
@@ -244,6 +308,17 @@ private async animateAllTargets(): Promise<void> {
     }
   }
 
+public Fly(): void {
+  this.isFlying = !this.isFlying;
+
+  if (this.isFlying && !this.animationId) {
+    this.startAnimationLoop();
+  }
+
+  this.flightLabel = this.isFlying ? "Stop" : "Start";
+}
+    
+
   private drawRoute(context: CanvasRenderingContext2D, points: Point[]): void {
     if (!points || points.length < 2) return;
 
@@ -263,7 +338,10 @@ private async animateAllTargets(): Promise<void> {
   private drawNeedles(points: Point[]): void {
     const canvas = this.myCanvas.nativeElement;
     const context = canvas.getContext('2d');
-    if (!context) return;
+    if (!context)
+    {
+       return;
+    }
 
     points.forEach(point => {
       context.beginPath();
@@ -279,5 +357,49 @@ private async animateAllTargets(): Promise<void> {
       context.textAlign = 'center';
       context.fillText(point.city, point.xPx, point.yPx - 8);
     });
+  }
+
+ 
+
+private drawRotatedPlane(
+  context: CanvasRenderingContext2D,
+  img: HTMLImageElement,
+  x: number,
+  y: number,
+  angle: number,
+  displayWidth: number = 50,
+  displayHeight: number = 50
+) {
+  context.save();
+  context.translate(x, y);
+  context.rotate(angle);
+  // Draw scaled image centered at (0,0)
+  context.drawImage(img, -displayWidth / 2, -displayHeight / 2, displayWidth, displayHeight);
+  context.restore();
+
+  console.log('Drawing plane at:', x, y, 'with angle:', angle);
+}
+  private drawPlane(point: Point, angle:number): void{
+
+    const canvas = this.myCanvas.nativeElement;
+    const context = canvas.getContext('2d');
+    if (!context)
+    {
+       return;
+    }
+
+    // context.save();
+    //  context.translate(point.xPx, point.yPx);
+    //context.rotate(angle);
+console.log('Drawing plane at:', point.xPx, point.yPx, 'with angle:', angle);
+
+    context.drawImage(this.plane, point.xPx - 10, point.yPx - 10, 5, 5);
+    // context.restore();
+  }
+
+  private getAngleBetweenPoints(p1: Point, p2: Point): number {
+    const deltaY = p2.yPx - p1.yPx;
+    const deltaX = p2.xPx - p1.xPx;
+    return Math.atan2(deltaY, deltaX) + Math.PI / 2;
   }
 }
